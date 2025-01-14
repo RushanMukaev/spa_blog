e07e530d9bf247b96d35b78817e33a990d596c2b
Rushan Mukaev <mukaefff@gmail.com>
2025-01-15 01:47:50 +0300

    App Init(install Laravel, Vue, Vue Router, Tailwind)

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+        <router-link to="index">index</router-link>
+        <router-link to="page">page</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/views/Index.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        index
+    </div>
+</template>
+
+<style scoped>
+
+</style>
